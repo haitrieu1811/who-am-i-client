f88fc466c66d961b828b7e909c12dd51dc5f889b
Tran Hai Trieu <haitrieu2524@gmail.com>
2025-07-25 17:29:31 +0700

    chore: Thêm Shadcn UI vào dự án.

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import App from './App'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    element: <App />
   }
 ])
 
