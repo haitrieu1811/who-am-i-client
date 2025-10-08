12c1b096d654543fa699e52bc5ce97a300760f5a
Tran Hai Trieu <haitrieu2524@gmail.com>
2025-10-08 17:01:14 +0700

    update: Cấu hình file vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   preview: {
     port: 3001,
-    allowedHosts: [process.env.VITE_CLIENT_HOST!]
+    allowedHosts: ['whoami.tranhaitrieu.com']
   },
   css: {
     devSourcemap: true
